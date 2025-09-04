3668ecaa3311436f2f23e9240b5a9d8e889e0bd9
heegu123 <kookminkhg@kookmin.ac.kr>
2025-09-04 12:48:07 +0900

    init: spring boot project

---
+++ src/main/java/com/peelie/PeelieApplication.java
@@ -0,0 +1,13 @@
+package com.peelie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeelieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeelieApplication.class, args);
+    }
+
+}

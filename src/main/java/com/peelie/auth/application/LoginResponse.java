d38b75f46b83c6d9b2d4460e4db99590f7620a3f
heegu123 <kookminkhg@kookmin.ac.kr>
2025-09-23 16:58:23 +0900

    chore: 필드명 변경 - 의미가 더 잘 드러나게 token -> jwt로 변경

--- src/main/java/com/peelie/auth/application/LoginResponse.java
+++ src/main/java/com/peelie/auth/application/LoginResponse.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public class LoginResponse {
-    private final String token;      // 토큰 (신규 유저일 경우 임시 토큰, 기존 유저일 경우 최종 토큰)
-    private final String tokenType;  // 토큰 타입 ("pending" or "completed")
+    private final String jwt;      // jwt 토큰 (신규 유저일 경우 임시 토큰, 기존 유저일 경우 최종 토큰)
+    private final String jwtType;  // jwt 토큰 타입 ("pending" or "completed")
     private final boolean isNewUser; // 신규 유저 여부
 
-    public LoginResponse(String token, String tokenType, boolean isNewUser) {
-        this.token = token;
-        this.tokenType = tokenType;
+    public LoginResponse(String jwt, String jwtType, boolean isNewUser) {
+        this.jwt = jwt;
+        this.jwtType = jwtType;
         this.isNewUser = isNewUser;
     }
 }

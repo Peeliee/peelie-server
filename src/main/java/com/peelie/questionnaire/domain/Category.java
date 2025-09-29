4d86d67c02489858a5fed06d7c271e304db3df17
heegu123 <kookminkhg@kookmin.ac.kr>
2025-09-29 17:35:22 +0900

    feat: Onboarding 카테고리 엔티티 추가
    - Category 엔티티 생성 (onboarding_category 테이블 매핑)
    - L0~L3 객관식 질문(ChoiceQuestion) 연관관계 설정
    - L4 서술형 질문 필드 추가(String)
    - 선택지 4개 검증 로직 및 L4 질문 수정 메서드 구현

---
+++ src/main/java/com/peelie/questionnaire/domain/Category.java
@@ -0,0 +1,48 @@
+package com.peelie.questionnaire.domain;
+
+import com.peelie.common.exception.BaseException;
+import com.peelie.common.exception.ErrorCode;
+import com.peelie.common.jpa.BaseTimeEntity;
+import com.peelie.questionnaire.domain.question.ChoiceQuestion;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "onboarding_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+    // L0 ~ L3 객관식 질문들
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChoiceQuestion> choiceQuestions = new ArrayList<>();
+
+    // L4 서술형 질문
+    private String l4Question;
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void addChoiceQuestion(ChoiceQuestion question) {
+        if (choiceQuestions.size() != 4) { // L0~L3
+            throw new BaseException("객관식 질문은 4개를 등록 해야합니다.", ErrorCode.VALIDATION_ERROR);
+        }
+        this.choiceQuestions.add(question);
+    }
+
+    public void updateL4Question(String l4Question) {
+        this.l4Question = l4Question;
+    }
+}

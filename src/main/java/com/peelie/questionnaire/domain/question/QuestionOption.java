0cbcadbc294ae40feffaf7acc8b6fc39c2b3ce4f
heegu123 <kookminkhg@kookmin.ac.kr>
2025-09-30 20:07:17 +0900

    feat: QuestionOption 엔티티 생성 (질문 선택지 관리)
    - QuestionOption 엔티티 생성
    - 'content' 필드 추가
    - Question과 ManyToOne 관계 설정
    - setQuestion 메서드 추가

---
+++ src/main/java/com/peelie/questionnaire/domain/question/QuestionOption.java
@@ -0,0 +1,23 @@
+package com.peelie.questionnaire.domain.question;
+
+import com.peelie.common.jpa.BaseTimeEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "question_options")
+public class QuestionOption extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}

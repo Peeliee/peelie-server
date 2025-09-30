69d2fc79bcfb9ea41b43bbcc688a2196b4f47e52
heegu123 <kookminkhg@kookmin.ac.kr>
2025-09-30 20:05:55 +0900

    feat: SubCategory 엔티티 생성 및 질문 관리 추가
    - SubCategory 엔티티 생성 (기존PRD L0 의 응답에 해당)
    - 'name' 필드
    - Category와 ManyToOne 관계 설정
    - Question과 OneToMany 관계 설정 및 addQuestion 메서드 구현

---
+++ src/main/java/com/peelie/questionnaire/domain/category/SubCategory.java
@@ -0,0 +1,45 @@
+package com.peelie.questionnaire.domain.category;
+
+import com.peelie.common.jpa.BaseTimeEntity;
+import com.peelie.questionnaire.domain.question.Question;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "onboarding_l0_options")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubCategory extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    // L1 ~ L3 객관식 질문들
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
+    public SubCategory(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void addQuestion(Question question) {
+        this.questions.add(question);
+        question.setSubCategory(this);
+    }
+}

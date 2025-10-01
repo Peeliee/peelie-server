51b8344692019b45c03f06bde77c192cc49313a4
heegu123 <kookminkhg@kookmin.ac.kr>
2025-10-01 17:46:07 +0900

    refactor: 목적에 맞게 디렉토리 위치 변경

--- src/main/java/com/peelie/profile/domain/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.peelie.profile.domain;
-
-import java.util.Optional;
-
-public interface ProfileRepository {
-    Profile save(Profile profile);
-    Optional<Profile> findByUserId(Long userId);
-}

90c70c3a73ef438203b8b3de2af6b519cd47291e
heegu123 <kookminkhg@kookmin.ac.kr>
2025-10-02 16:24:44 +0900

    refactor: QuestionnaireServiceImpl 간소화
    - getCategoryByName, getSubCategoryByName, L1~L4 질문 조회 메서드 제거
    - getCategoryInfoById, getQuestionsByIds 중심으로 최소 기능만 유지

--- src/main/java/com/peelie/questionnaire/domain/QuestionnaireServiceImpl.java
+++ src/main/java/com/peelie/questionnaire/domain/QuestionnaireServiceImpl.java
@@ -1,14 +1,12 @@
 package com.peelie.questionnaire.domain;
 
-import com.peelie.common.exception.BaseException;
-import com.peelie.common.exception.ErrorCode;
 import com.peelie.questionnaire.domain.category.*;
 import com.peelie.questionnaire.domain.question.QuestionInfo;
-import com.peelie.questionnaire.domain.question.QuestionLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,88 +25,16 @@ public class QuestionnaireServiceImpl implements QuestionnaireService{
     }
 
     @Override
-    public CategoryInfo getCategory(Long categoryId) {
-        Category category = categoryReader.getCategory(categoryId);
-
-        return new CategoryInfo(category);
+    public CategoryInfo getCategoryInfoById(Long categoryId) {
+        return new CategoryInfo(categoryReader.getCategory(categoryId));
     }
 
     @Override
-    public CategoryInfo getCategoryByName(String categoryName) {
-        Category category = categoryReader.getCategoryByName(categoryName);
+    public List<QuestionInfo> getQuestionsByIds(Long categoryId, Long subCategoryId) {
+        SubCategory subCategory = subCategoryReader.getSubCategoryByIds(categoryId, subCategoryId);
 
-        return new CategoryInfo(category);
-    }
-
-    @Override
-    public List<SubCategoryInfo> getSubCategories(Long categoryId) {
-        Category category = categoryReader.getCategory(categoryId);
-        List<SubCategory> subCategories = category.getSubCategories();
-
-        return subCategories.stream()
-                .map(SubCategoryInfo::new)
-                .toList();
-    }
-
-    @Override
-    public SubCategoryInfo getSubCategory(Long subCategoryId) {
-        SubCategory subCategory = subCategoryReader.getSubCategory(subCategoryId);
-
-        return new SubCategoryInfo(subCategory);
-    }
-
-    @Override
-    public SubCategoryInfo getSubCategoryByName(String subCategoryName) {
-        SubCategory subCategoryByName = subCategoryReader.getSubCategoryByName(subCategoryName);
-
-        return new SubCategoryInfo(subCategoryByName);
-    }
-
-    @Override
-    public QuestionInfo getL1Question(String subCategoryName) {
-        SubCategory subCategory = subCategoryReader.getSubCategoryByName(subCategoryName);
-        QuestionInfo questionInfo = subCategory.getQuestions().stream()
-                .filter(question -> question.getLevel() == QuestionLevel.L1)
+        return subCategory.getQuestions().stream()
                 .map(QuestionInfo::new)
-                .findFirst()
-                .orElseThrow(() -> new BaseException(subCategoryName + "해당 서브 카테고리가 존재하지 않습니다.", ErrorCode.NOT_FOUND));
-
-        return questionInfo;
-    }
-
-    @Override
-    public QuestionInfo getL2Question(String subCategoryName) {
-        SubCategory subCategory = subCategoryReader.getSubCategoryByName(subCategoryName);
-        QuestionInfo questionInfo = subCategory.getQuestions().stream()
-                .filter(question -> question.getLevel() == QuestionLevel.L2)
-                .map(QuestionInfo::new)
-                .findFirst()
-                .orElseThrow(() -> new BaseException(subCategoryName + "해당 서브 카테고리가 존재하지 않습니다.", ErrorCode.NOT_FOUND));
-
-        return questionInfo;
-    }
-
-    @Override
-    public QuestionInfo getL3Question(String subCategoryName) {
-        SubCategory subCategory = subCategoryReader.getSubCategoryByName(subCategoryName);
-        QuestionInfo questionInfo = subCategory.getQuestions().stream()
-                .filter(question -> question.getLevel() == QuestionLevel.L3)
-                .map(QuestionInfo::new)
-                .findFirst()
-                .orElseThrow(() -> new BaseException(subCategoryName + "해당 서브 카테고리가 존재하지 않습니다.", ErrorCode.NOT_FOUND));
-
-        return questionInfo;
-    }
-
-    @Override
-    public QuestionInfo getL4Question(String subCategoryName) {
-        SubCategory subCategory = subCategoryReader.getSubCategoryByName(subCategoryName);
-        QuestionInfo questionInfo = subCategory.getQuestions().stream()
-                .filter(question -> question.getLevel() == QuestionLevel.L4)
-                .map(QuestionInfo::new)
-                .findFirst()
-                .orElseThrow(() -> new BaseException(subCategoryName + "해당 서브 카테고리가 존재하지 않습니다.", ErrorCode.NOT_FOUND));
-
-        return questionInfo;
+                .collect(Collectors.toList());
     }
 }

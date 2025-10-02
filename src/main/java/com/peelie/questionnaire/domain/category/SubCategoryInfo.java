f986debcd18235fdfe05e17c87b977724d67b1f5
heegu123 <kookminkhg@kookmin.ac.kr>
2025-10-02 16:21:52 +0900

    refactor: SubCategoryInfo 단순화
    - 불필요한 categoryName, questions 제거
    - id, name 필드만 남기도록 DTO 축소

--- src/main/java/com/peelie/questionnaire/domain/category/SubCategoryInfo.java
+++ src/main/java/com/peelie/questionnaire/domain/category/SubCategoryInfo.java
@@ -1,25 +1,15 @@
 package com.peelie.questionnaire.domain.category;
 
-import com.peelie.questionnaire.domain.question.QuestionInfo;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 public class SubCategoryInfo {
 
-    private final Long subCategoryId;
-    private final String categoryName;
-    private final String SubCategoryName;
-    private final List<QuestionInfo> questions;
+    private final Long id;
+    private final String name;
 
     public SubCategoryInfo(SubCategory subCategory) {
-
-        this.subCategoryId = subCategory.getId();
-        this.categoryName = subCategory.getCategory().getName();
-        this.SubCategoryName = subCategory.getName();
-        this.questions = subCategory.getQuestions().stream()
-                .map(QuestionInfo::new)
-                .toList();
+        this.id = subCategory.getId();
+        this.name = subCategory.getName();
     }
 }

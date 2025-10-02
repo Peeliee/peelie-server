d9fe1b4691b37862c92f454307f5cca46d37ef72
heegu123 <kookminkhg@kookmin.ac.kr>
2025-10-02 16:25:06 +0900

    refactor: QuestionnaireAppService 단순화 및 조회 방식 변경
    - getCategory, getSubCategory, L1~L4 개별 질문 조회 메서드 제거
    - getAllCategories, getCategoryInfoById, getQuestionsByIds 중심으로 구성
    - 조회 기준을 이름 기반에서 ID 기반으로 전환

--- src/main/java/com/peelie/questionnaire/application/QuestionnaireAppService.java
+++ src/main/java/com/peelie/questionnaire/application/QuestionnaireAppService.java
@@ -2,7 +2,6 @@ package com.peelie.questionnaire.application;
 
 import com.peelie.questionnaire.domain.QuestionnaireService;
 import com.peelie.questionnaire.domain.category.CategoryInfo;
-import com.peelie.questionnaire.domain.category.SubCategoryInfo;
 import com.peelie.questionnaire.domain.question.QuestionInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,34 +14,15 @@ public class QuestionnaireAppService {
 
    private final QuestionnaireService questionnaireService;
 
-   /*
-    /*
-    1. 카테고리 조회 -> 서브 카테고리 나옴
-    2. 카테고리, 서브카테고리 이름 두개 입력 하면 -> L0~L4 질문 나옴
-     */
-
-    public CategoryInfo getCategory(String categoryName) {
-        return questionnaireService.getCategoryByName(categoryName);
+    public List<CategoryInfo> getAllCategories() {
+        return questionnaireService.getAllCategories();
     }
 
-    public List<QuestionInfo> getSubCategory(String subCategoryName) {
-        SubCategoryInfo subCategoryInfo = questionnaireService.getSubCategoryByName(subCategoryName);
-        return subCategoryInfo.getQuestions();
+    public CategoryInfo getL0QuestionAndChoicesById(Long categoryId) {
+        return questionnaireService.getCategoryInfoById(categoryId);
     }
 
-    public QuestionInfo getL1Question(String subCategoryName) {
-        return questionnaireService.getL1Question(subCategoryName);
-    }
-
-    public QuestionInfo getL2Question(String subCategoryName) {
-        return questionnaireService.getL2Question(subCategoryName);
-    }
-
-    public QuestionInfo getL3Question(String subCategoryName) {
-        return questionnaireService.getL3Question(subCategoryName);
-    }
-
-    public QuestionInfo getL4Question(String subCategoryName) {
-        return questionnaireService.getL4Question(subCategoryName);
+    public List<QuestionInfo> getL1ToL4QuestionsByIds(Long categoryId, Long subCategoryId) {
+        return questionnaireService.getQuestionsByIds(categoryId, subCategoryId);
     }
 }

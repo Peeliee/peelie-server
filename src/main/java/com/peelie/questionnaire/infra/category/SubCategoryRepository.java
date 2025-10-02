5f846fed842ca7a5877757c65bf3cec529b09548
heegu123 <kookminkhg@kookmin.ac.kr>
2025-10-02 16:23:05 +0900

    feat: SubCategory 조회 메서드 추가
    - categoryId + subCategoryId 기반 조회 메서드 findByIds 추가
    - 부모 카테고리와 서브카테고리 ID가 모두 일치하는 경우만 반환

--- src/main/java/com/peelie/questionnaire/infra/category/SubCategoryRepository.java
+++ src/main/java/com/peelie/questionnaire/infra/category/SubCategoryRepository.java
@@ -2,9 +2,15 @@ package com.peelie.questionnaire.infra.category;
 
 import com.peelie.questionnaire.domain.category.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
-    Optional<SubCategory> findByName(String subCategoryName);
+    // 서브카테고리 ID와 부모 카테고리 ID가 모두 일치하는 레코드 조회
+    @Query("SELECT sc " +
+            "FROM SubCategory sc " +
+            "WHERE sc.id = :subCategoryId AND sc.category.id = :categoryId")
+    Optional<SubCategory> findByIds(@Param("categoryId") Long categoryId, @Param("subCategoryId") Long subCategoryId);
 }

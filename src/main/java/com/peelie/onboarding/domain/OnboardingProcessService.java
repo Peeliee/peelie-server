c54293cdfeb8ad125ab4449e9a4e7bc9f34b610b
seonghabin <seonghabin@gmail.com>
2025-10-07 19:34:46 +0900

    feat(onboarding): 온보딩 질문 답변 검증 및 저장 서비스 구현

--- src/main/java/com/peelie/onboarding/domain/OnboardingProcessService.java
+++ src/main/java/com/peelie/onboarding/domain/OnboardingProcessService.java
@@ -2,4 +2,5 @@ package com.peelie.onboarding.domain;
 
 public interface OnboardingProcessService {
     OnboardingInfo.Process selectCategories(OnboardingCommand.SelectCategories command);
+    OnboardingInfo.Process submitAnswer(OnboardingCommand.SubmitAnswer command);
 }

d40e7063764619779b0aa95e69d1668f3e292027
seonghabin <seonghabin@gmail.com>
2025-10-07 18:29:17 +0900

    refactor(onboarding): ProcessInfo 생성자 추가

--- src/main/java/com/peelie/onboarding/domain/OnboardingInfo.java
+++ src/main/java/com/peelie/onboarding/domain/OnboardingInfo.java
@@ -16,12 +16,28 @@ public class OnboardingInfo {
         private OnboardingStatus status;
         private Set<Long> selectedCategoryIds;
         private List<Answer> answers;
+
+        public Process(OnboardingProcess process) {
+            this.processId = process.getId();
+            this.userId = process.getUserId();
+            this.status = process.getStatus();
+            this.selectedCategoryIds = process.getSelectedCategories();
+            this.answers = process.getAnswers().stream()
+                    .map(ans -> new Answer(ans.getQuestionId(), ans.getValue()))
+                    .toList();
+        }
     }
 
     @Getter
     @Builder
     public static class Answer{ //개별 답변 단위로 관리
         private Long questionId;
+        private String value;
+
+        public Answer(Long questionId, String value) {
+            this.questionId = questionId;
+            this.value = value;
+        }
     }
 }
 

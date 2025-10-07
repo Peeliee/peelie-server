9a77cf6d6c46f8dda050b23b8ae5f6a1c17f34c8
seonghabin <seonghabin@gmail.com>
2025-10-07 18:35:48 +0900

    feat(onboarding): SubmitAnswer Command 추가

--- src/main/java/com/peelie/onboarding/domain/OnboardingCommand.java
+++ src/main/java/com/peelie/onboarding/domain/OnboardingCommand.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.util.List;
 import java.util.Set;
 
 public class OnboardingCommand {
@@ -15,6 +14,15 @@ public class OnboardingCommand {
     public static class SelectCategories {
         private Long userId;
         private Set<Long> categoryIds;
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class SubmitAnswer {
+        private Long userId; //도메인 엔티티에는 포함되지 않지만 특정 사용자의 온보딩 프로세스를 식별하기 위해 요청 DTO 에서만 사용
+        private Long questionId;
+        private String value;
     }
 
 

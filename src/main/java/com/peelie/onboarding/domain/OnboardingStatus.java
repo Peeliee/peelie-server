6a8b5939edfb8f02b850714e6fe7da343f262f95
seonghabin <seonghabin@gmail.com>
2025-10-08 14:56:27 +0900

    refactor(onboarding): profile 도메인에서의 필드를 맞추기 위해 이름 변경 (InterationType -> InterationStyle)

--- src/main/java/com/peelie/onboarding/domain/OnboardingStatus.java
+++ src/main/java/com/peelie/onboarding/domain/OnboardingStatus.java
@@ -3,6 +3,6 @@ package com.peelie.onboarding.domain;
 public enum OnboardingStatus {
     CATEGORIES_PENDING, // 카테고리 선택 대기
     QUESTIONS_PENDING,  // 질문 답변 대기
-    INTERACTIONTYPE_PENDING, // 교류 성향, 한줄 소개 대기
+    INTERACTIONSTYLE_PENDING, // 교류 성향, 한줄 소개 대기
     COMPLETED           // 온보딩 질문 답변 완료
 }

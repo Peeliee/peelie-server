a845474f8e37e7b382940040e663123e0c9159e2
seonghabin <seonghabin@gmail.com>
2025-10-10 03:59:21 +0900

    feat(onboarding):  인터페이스 레이어(OnboardingController) 구현 완료

---
+++ src/main/java/com/peelie/onboarding/interfaces/OnboardingController.java
@@ -0,0 +1,38 @@
+package com.peelie.onboarding.interfaces;
+
+import com.peelie.common.response.SuccessResponse;
+import com.peelie.onboarding.application.OnboardingFacade;
+import com.peelie.onboarding.domain.OnboardingCommand;
+import com.peelie.onboarding.domain.OnboardingInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/onboarding")
+@RequiredArgsConstructor
+public class OnboardingController {
+
+    private final OnboardingFacade onboardingFacade;
+
+    @PostMapping("/categories")
+    public SuccessResponse<OnboardingInfo.Process> selectCategories(@RequestBody OnboardingCommand.SelectCategories command) {
+        OnboardingInfo.Process result = onboardingFacade.selectCategories(command);
+        return SuccessResponse.ok(result);
+    }
+
+    @PostMapping("/answers")
+    public SuccessResponse<OnboardingInfo.Process> submitAnswer(@RequestBody OnboardingCommand.SubmitAnswer command) {
+        OnboardingInfo.Process result = onboardingFacade.submitAnswer(command);
+        return SuccessResponse.ok(result);
+    }
+
+    @PostMapping("/interaction")
+    public SuccessResponse<OnboardingInfo.Process> submitInteractionStyle(@RequestBody OnboardingCommand.SubmitInteraction command) {
+        OnboardingInfo.Process result = onboardingFacade.submitInteractionStyle(command);
+        return SuccessResponse.ok(result);
+    }
+
+}

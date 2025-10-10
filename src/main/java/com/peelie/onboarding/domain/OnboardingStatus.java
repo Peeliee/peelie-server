71963921ac406f5c0dca1b03f6de2cf8affe109a
seonghabin <seonghabin@gmail.com>
2025-10-10 22:04:29 +0900

    feat(onboarding): status enum INITIALIZED 추가

--- src/main/java/com/peelie/onboarding/domain/OnboardingStatus.java
+++ src/main/java/com/peelie/onboarding/domain/OnboardingStatus.java
@@ -1,6 +1,7 @@
 package com.peelie.onboarding.domain;
 
 public enum OnboardingStatus {
+    INITIALIZED, //초기 상태
     CATEGORIES_PENDING, // 카테고리 선택 대기
     QUESTIONS_PENDING,  // 질문 답변 대기
     INTERACTIONSTYLE_PENDING, // 교류 성향, 한줄 소개 대기

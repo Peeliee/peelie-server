11aee20c5278445c5ccf7633e4234a3ba0e9578c
heegu123 <kookminkhg@kookmin.ac.kr>
2025-10-13 15:31:24 +0900

    feat(config): LoginCheckInterceptor WebConfig에 등록
    - 모든 경로에 대해 인터셉터 적용 (`/**`)
    - 로그인 및 설문 관련 엔드포인트는 제외 (`/api/v1/oauth/login/**`, `/api/v1/questionnaire/**`)

---
+++ src/main/java/com/peelie/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.peelie.common.config;
+
+import com.peelie.common.interceptor.LoginCheckInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginCheckInterceptor loginCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/v1/oauth/login/**", "/api/v1/questionnaire/**");
+    }
+}

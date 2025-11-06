a081cfe57d971466a4db6ba08b1e6e4ea7af83c6
heegu123 <kookminkhg@kookmin.ac.kr>
2025-11-06 17:07:22 +0900

    refactor(profile): ProfileFacade 수정
    - registerProfile 파라미터를 ProfileCommand.RegisterCommand로 수정
    - 프로필 수정 로직을 updateProfile(UpdateCommand) 단일 메서드로 통합
    - 기존 개별 update 메서드(updateProfileName, updateInstagramId, updateImageUrl) 제거

--- src/main/java/com/peelie/profile/application/ProfileFacade.java
+++ src/main/java/com/peelie/profile/application/ProfileFacade.java
@@ -13,7 +13,7 @@ public class ProfileFacade {
     private final ProfileService profileService;
 
     // 프로필 생성
-    public ProfileInfo registerProfile(ProfileCommand command) {
+    public ProfileInfo registerProfile(ProfileCommand.RegisterCommand command) {
         return profileService.registerProfile(command);
     }
 
@@ -25,16 +25,8 @@ public class ProfileFacade {
 
     // 프로필 수정
     // 프로필 이름 수정
-    public void updateProfileName(Long userId, String newProfileName) {
-        profileService.updateProfileName(userId, newProfileName);
-    }
-    // 프로필 인스타 ID 수정
-    public void updateInstagramId(Long userId, String newInstagramId) {
-        profileService.updateInstagramId(userId, newInstagramId);
-    }
-    // 프로필 이미지 수정
-    public void updateImageUrl(Long userId, String newImageUrl) {
-        profileService.updateProfileImage(userId, newImageUrl);
+    public ProfileInfo updateProfile(Long userId, ProfileCommand.UpdateCommand command) {
+        return profileService.updateMyProfile(userId, command);
     }
 
     // 프로필 사진 리셋

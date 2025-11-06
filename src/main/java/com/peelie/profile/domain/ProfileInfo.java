a61eca42d46a4be28c3023e253a78d648e3b1dbe
heegu123 <kookminkhg@kookmin.ac.kr>
2025-11-06 17:04:09 +0900

    feat(profile): ProfileInfo 확장, bio·card 데이터 구조 추가
    - ProfileInfo에 bio를 단계별 BioInfo 리스트로 구성하도록 변경
    - stage0~3 bio 포함 및 STAGE0_BIO 기본 메시지 추가
    - Card 및 StageInfo 내부 static 클래스로 작성하여 응답 구조 확장
    - 기존 단일 bio 필드 및 profileId 제거

--- src/main/java/com/peelie/profile/domain/ProfileInfo.java
+++ src/main/java/com/peelie/profile/domain/ProfileInfo.java
@@ -1,24 +1,66 @@
 package com.peelie.profile.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class ProfileInfo {
-    private final Long profileId;
     private final Long userId;
     private final String userName;
     private final String profileImageUrl;
     private final String instagramId;
-    private final String bio;
+    private final List<BioInfo> bio;
     private final InteractionStyle interactionStyle;
+    private final Card card;
 
     public ProfileInfo(Profile profile) {
-        this.profileId = profile.getId();
         this.userId = profile.getUserId();
         this.userName = profile.getUserName();
         this.profileImageUrl = profile.getProfileImageUrl();
         this.instagramId = profile.getInstagramId();
-        this.bio = profile.getBio();
+
+        this.bio = List.of(
+                new BioInfo("stage0", BioInfo.STAGE0_BIO),
+                new BioInfo("stage1", profile.getStage1Bio()),
+                new BioInfo("stage2", profile.getStage2Bio()),
+                new BioInfo("stage3", profile.getStage3Bio())
+                );
+
         this.interactionStyle = profile.getInteractionStyle();
+
+        //TODO: 재현님 카드 기능 완성 후 실제 데이터 반영
+        this.card = new Card(
+                new Card.StageInfo("임시 Stage1 Title", "Stage1 Subtitle", "Stage1 Content"),
+                new Card.StageInfo("임시 Stage2 Title", "Stage2 Subtitle", "Stage1 Content"),
+                new Card.StageInfo("임시 Stage3 Title", "Stage3 Subtitle", "Stage1 Content")
+        );
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class BioInfo {
+        // TODO: 임시 메시지 나중에 실제 자기소개 메시지 나오면 변경
+        public static final String STAGE0_BIO = "자기소개가 없어요 퀴즈를 풀어주세요.";
+
+        private final String stage;
+        private final String bio;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Card {
+        private final StageInfo stage1;
+        private final StageInfo stage2;
+        private final StageInfo stage3;
+
+        @Getter
+        @AllArgsConstructor
+        public static class StageInfo {
+            private final String title;
+            private final String subtitle;
+            private final String content;
+        }
     }
 }

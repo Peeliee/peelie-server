31aadf54023a5b6b2ab7ba9438edb70d1f422532
gwondoo <6568kdh@naver.com>
2025-11-10 18:41:23 +0900

    refactor: 내부 함수 이름 수정

--- src/main/java/com/peelie/friendship/domain/FriendshipService.java
+++ src/main/java/com/peelie/friendship/domain/FriendshipService.java
@@ -1,6 +1,6 @@
 package com.peelie.friendship.domain;
 
-public interface x {
+public interface FriendshipService {
     FriendshipInfo.CreateFriendship createFriendship(FriendshipCommand.CreateFriendship command);
     FriendshipInfo.GetFriendList getFriendList(FriendshipCommand.GetFriendList command);
 }

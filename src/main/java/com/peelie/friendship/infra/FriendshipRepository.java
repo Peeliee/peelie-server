77ce8486dd26c2cd19d9670d6931bbed619476f6
gwondoo <6568kdh@naver.com>
2025-11-10 20:31:32 +0900

    refactor: 레포지토리 친구 조회 리스트로 변경

--- src/main/java/com/peelie/friendship/infra/FriendshipRepository.java
+++ src/main/java/com/peelie/friendship/infra/FriendshipRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
-     List<Friendship> getFriendshipById(Long userId);
+     List<Long> getFriendshipById(Long userId);
 }

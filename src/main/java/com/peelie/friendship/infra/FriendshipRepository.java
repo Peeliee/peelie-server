a864e4d0fca68a9eb50be7eee1e78e59d8a26e29
gwondoo <6568kdh@naver.com>
2025-11-10 20:15:13 +0900

    feat: repository 파일 생성

--- src/main/java/com/peelie/friendship/infra/FriendshipRepository.java
+++ src/main/java/com/peelie/friendship/infra/FriendshipRepository.java
@@ -3,6 +3,8 @@ package com.peelie.friendship.infra;
 import com.peelie.friendship.domain.Friendship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+import java.util.Optional;
 
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+     Optional<Friendship> getFriendshipById(Long userId);
 }

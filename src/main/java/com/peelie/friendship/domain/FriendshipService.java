e76e26a051629a90a6ac64e1b452a8732b5b42fe
gwondoo <6568kdh@naver.com>
2025-11-14 17:35:43 +0900

    refactor: 변경된 인포 이름으로 변경

--- src/main/java/com/peelie/friendship/domain/FriendshipService.java
+++ src/main/java/com/peelie/friendship/domain/FriendshipService.java
@@ -1,8 +1,8 @@
 package com.peelie.friendship.domain;
 
 public interface FriendshipService {
-    FriendshipInfo.CreateFriendship createFriendship(Long receiverId, Long senderId);
+    FriendshipInfo.FriendDetail createFriendship(Long receiverId, Long senderId);
     FriendshipInfo.FriendListResponse getFriendList(Long userid);
-    FriendshipInfo.GetFriendDetail  getFriendDetail(Long userid);
+    FriendshipInfo.FriendDetail  getFriendDetail(Long userid);
     FriendshipInfo.RandomFriendResponse  getRandomFriend(Long userid);
 }

37b1a04021499a69cb8e3b986ab288e3d3030fad
gwondoo <6568kdh@naver.com>
2025-11-14 16:51:18 +0900

    refactor: 리더에서 페어값 찾는거 추가

--- src/main/java/com/peelie/friendship/domain/FriendshipReader.java
+++ src/main/java/com/peelie/friendship/domain/FriendshipReader.java
@@ -3,5 +3,7 @@ package com.peelie.friendship.domain;
 import java.util.List;
 
 public interface FriendshipReader {
-    List<Long> findFriendUserIds(Long userId);
+    List<Long> findFriendByUserIds(Long userId);
+
+    boolean existPair(Long a, Long b);
 }

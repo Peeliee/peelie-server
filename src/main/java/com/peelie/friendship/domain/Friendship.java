fd00549d1bd76afb12bfb259a43e1f9ed07aafc5
gwondoo <6568kdh@naver.com>
2025-11-14 16:50:22 +0900

    refactor: stage 엔티티 추가

--- src/main/java/com/peelie/friendship/domain/Friendship.java
+++ src/main/java/com/peelie/friendship/domain/Friendship.java
@@ -1,14 +1,13 @@
 package com.peelie.friendship.domain;
 
 import com.peelie.common.jpa.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -19,14 +18,22 @@ public class Friendship extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    Long senderId;
-    Long receiverId;
+    private Long senderId;
+    private Long receiverId;
+
+    // 스테이지 이넘값
+    @Enumerated(EnumType.STRING)
+    private FriendStage friendStage;
 
     @Builder
     public Friendship(Long senderId, Long receiverId) {
         this.senderId = senderId;
         this.receiverId = receiverId;
+        this.friendStage = FriendStage.DEFAULT;
     }
 
+    public void changeFriendStage(FriendStage newfriendStage) {
+        this.friendStage = Objects.requireNonNull(newfriendStage);
+    }
 }
 

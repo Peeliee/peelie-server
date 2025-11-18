e6755df624f2250e5e40813ce833532f13c87838
seonghabin <seonghabin@gmail.com>
2025-11-18 22:11:29 +0900

    feat(friendship): 짝이 존재하는지 여부뿐만 아니라 객체 반환하는 reader 생성

--- src/main/java/com/peelie/friendship/domain/FriendshipReader.java
+++ src/main/java/com/peelie/friendship/domain/FriendshipReader.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface FriendshipReader {
     List<Long> findFriendsByUserId(Long userId);
     boolean existPair(Long a, Long b);
+    Friendship getByPair(Long a, Long b);
 }

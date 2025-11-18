a7a42fa8281421c2be8dd1d04a9f7ed6a854fa1a
gwondoo <6568kdh@naver.com>
2025-11-18 13:48:14 +0900

    feat: 프로필 리스트 반환 -> n+1 방지용

--- src/main/java/com/peelie/profile/domain/ProfileReader.java
+++ src/main/java/com/peelie/profile/domain/ProfileReader.java
@@ -1,6 +1,9 @@
 package com.peelie.profile.domain;
 
+import java.util.List;
+
 public interface ProfileReader {
     Profile getProfile(Long id);
     Profile getProfileByUserId(Long userId);
+    List<Profile> getProfilesByUserIds(List<Long> userIds);
 }

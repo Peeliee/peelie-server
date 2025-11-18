fcfb9a3c1581066089e2b4c4bdb1f9e67bc7b390
gwondoo <6568kdh@naver.com>
2025-11-18 15:16:16 +0900

    feat: 프렌드 애플리케이션 레이어에서 파사드 작성

--- src/main/java/com/peelie/friendship/application/FriendshipFacade.java
+++ src/main/java/com/peelie/friendship/application/FriendshipFacade.java
@@ -1,4 +1,34 @@
 package com.peelie.friendship.application;
 
+
+import com.peelie.friendship.domain.FriendshipInfo;
+import com.peelie.friendship.domain.FriendshipService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class FriendshipFacade {
+
+    private final FriendshipService friendshipService;
+
+    // 친구 생성
+    public FriendshipInfo.FriendDetail createFriendship(Long senderId, Long receiverId) {
+        return friendshipService.createFriendship(senderId, receiverId);
+    }
+
+    // 친구 목록 조회
+    public FriendshipInfo.FriendListResponse getFriendList(Long userId) {
+        return friendshipService.getFriendList(userId);
+    }
+
+    // 친구 상세 조회
+    public FriendshipInfo.FriendDetail getFriendDetail(Long userId) {
+        return friendshipService.getFriendDetail(userId);
+    }
+
+    // 랜덤 친구 5명 조회
+    public FriendshipInfo.RandomFriendResponse getRandomFriend(Long userId) {
+        return friendshipService.getRandomFriend(userId);
+    }
 }

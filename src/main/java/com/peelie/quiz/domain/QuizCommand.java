07573fb5c67de0179e8d1dfadf8c8d857ed20bea
heegu123 <kookminkhg@kookmin.ac.kr>
2025-11-28 17:28:53 +0900

    feat(quiz): QuizCommand 클래스 추가
    - 처음 제미나이 응답으로 받아온 Quiz는 Quiz엔티티를 생성할 때 필요한 값들 이므로 QuizInfo 는 프로젝트 규칙상 부적절함
    - 더 목적에 맞는 QuizCommand 클래스 추가

---
+++ src/main/java/com/peelie/quiz/domain/QuizCommand.java
@@ -0,0 +1,23 @@
+package com.peelie.quiz.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuizCommand {
+
+    private QuizStage quizStage;
+    private String question;
+    private String rightAnswer;
+    private String wrongAnswer;
+
+    public Quiz toEntity(Long userId) {
+        return Quiz.builder()
+                .userId(userId)
+                .stage(quizStage)
+                .rightAnswer(rightAnswer)
+                .wrongAnswer(wrongAnswer)
+                .build();
+    }
+}

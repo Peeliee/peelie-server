aa2691c547392fbe8a512822c4416cd0b6677a4a
heegu123 <kookminkhg@kookmin.ac.kr>
2025-11-28 17:52:18 +0900

    feat(quiz): QuizDoc 리턴타입 수정

--- src/main/java/com/peelie/quiz/interfaces/QuizDoc.java
+++ src/main/java/com/peelie/quiz/interfaces/QuizDoc.java
@@ -8,5 +8,5 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public interface QuizDoc {
 
     @Operation(summary = "퀴즈 생성", description = "현재 로그인한 사용자온보딩 답변 바탕으로 퀴즈 생성 후 DB에 저장")
-    SuccessResponse createUserQuiz();
+    SuccessResponse<String> createUserQuiz();
 }

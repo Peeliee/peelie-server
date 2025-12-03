6f0b80bc699832978dab720e22a8ce077bc92a12
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-04 01:14:10 +0900

    refactor: 불필요한 조인 삭제

--- src/main/java/com/peelie/onboarding/infra/OnboardingProcessRepository.java
+++ src/main/java/com/peelie/onboarding/infra/OnboardingProcessRepository.java
@@ -2,13 +2,9 @@ package com.peelie.onboarding.infra;
 
 import com.peelie.onboarding.domain.OnboardingProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface OnboardingProcessRepository extends JpaRepository<OnboardingProcess, Long> {
-    //TODO: ??? 이거 조인 하는 이유가 없는 듯
-    @Query("SELECT o FROM OnboardingProcess o LEFT JOIN FETCH o.subCategoryAnswers WHERE o.userId = :userId")
-    Optional<OnboardingProcess> findByUserId(@Param("userId")  Long userId);
+    Optional<OnboardingProcess> findByUserId(Long userId);
 }

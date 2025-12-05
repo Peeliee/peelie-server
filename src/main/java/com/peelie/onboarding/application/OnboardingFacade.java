77186c98ef6f5281e8b3fcef16920c9169cfe718
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-05 13:56:39 +0900

    feat: 프로필 저장 반영 안된 카드, 한줄소개 생성응용 서비스 메서드 작성

--- src/main/java/com/peelie/onboarding/application/OnboardingFacade.java
+++ src/main/java/com/peelie/onboarding/application/OnboardingFacade.java
@@ -1,11 +1,11 @@
 package com.peelie.onboarding.application;
 
+import com.peelie.onboarding.domain.GeneratedCardBio;
 import com.peelie.onboarding.domain.OnboardingCommand;
 import com.peelie.onboarding.domain.OnboardingInfo;
 import com.peelie.onboarding.domain.OnboardingProcessService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +23,10 @@ public class OnboardingFacade {
 
     public OnboardingInfo.Process submitInteractionStyle(OnboardingCommand.SubmitInteraction command) {
         return onboardingProcessService.submitInteractionStyle(command);
+    }
+
+    public GeneratedCardBio generateInitCards(Long userId) {
+        return onboardingProcessService.generateCardBio(userId);
+        // TODO: 여기에서 프로필에 저장하거나, 프로필에서 온보딩 서비스 코드를 호출할 지 추후 결정
     }
 }

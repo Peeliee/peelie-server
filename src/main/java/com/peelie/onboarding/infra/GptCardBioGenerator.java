138854bc1737843c7a9fb34fc923f2170fb4a063
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-05 13:58:56 +0900

    feat: openai-java의 StructuredResponseCreateParams.Builder 기능 사용하여 카드 및 한줄소개 구현체 추가
    - Chat Completions API이 아닌, Responses API 사용

---
+++ src/main/java/com/peelie/onboarding/infra/GptCardBioGenerator.java
@@ -0,0 +1,45 @@
+package com.peelie.onboarding.infra;
+
+import com.openai.client.OpenAIClient;
+import com.openai.models.ChatModel;
+import com.openai.models.responses.ResponseCreateParams;
+import com.openai.models.responses.StructuredResponse;
+import com.openai.models.responses.StructuredResponseCreateParams;
+import com.peelie.common.exception.BaseException;
+import com.peelie.common.exception.ErrorCode;
+import com.peelie.onboarding.domain.CardBioGenerator;
+import com.peelie.onboarding.domain.GeneratedCardBio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static com.peelie.onboarding.infra.OpenAIConfig.CARD_BIO_SYSTEM_PROMPT;
+
+@Component
+@RequiredArgsConstructor
+public class GptCardBioGenerator implements CardBioGenerator {
+
+    private final OpenAIClient openAIClient;
+
+    @Override
+    public GeneratedCardBio generate(String userPrompt) {
+
+        StructuredResponseCreateParams<GeneratedCardBio> params = ResponseCreateParams.builder()
+                .model(ChatModel.GPT_4O_MINI)
+                .input(CARD_BIO_SYSTEM_PROMPT + "\n" + userPrompt)
+                .text(GeneratedCardBio.class)
+                .maxOutputTokens(2048)
+                .temperature(0.0)
+                .build();
+
+        StructuredResponse<GeneratedCardBio> response = openAIClient.responses().create(params);
+
+        System.out.println("response.toString() = " + response.toString());
+
+        return response.output().stream()
+                .flatMap(item -> item.message().stream())
+                .flatMap(message -> message.content().stream())
+                .flatMap(content -> content.outputText().stream())
+                .findFirst()
+                .orElseThrow(() -> new BaseException("OpenAI API 응답 처리 중 오류가 발생했습니다.", ErrorCode.INTERNAL_SERVER_ERROR));
+    }
+}

830efaf39431b522ffd8c2c0039cd6852251948e
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-11 16:31:07 +0900

    feat: 프롬프트 생성을 facade에서 처리하도록 수정, 프로필에 온보딩에서 설정한 값 반영하는 메서드 추가

--- src/main/java/com/peelie/onboarding/application/OnboardingFacade.java
+++ src/main/java/com/peelie/onboarding/application/OnboardingFacade.java
@@ -4,14 +4,21 @@ import com.peelie.onboarding.domain.GeneratedCardBio;
 import com.peelie.onboarding.domain.OnboardingCommand;
 import com.peelie.onboarding.domain.OnboardingInfo;
 import com.peelie.onboarding.domain.OnboardingProcessService;
+import com.peelie.profile.domain.ProfileCommand;
+import com.peelie.profile.domain.ProfileService;
+import com.peelie.prompt.PromptGenerator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OnboardingFacade {
-    private final OnboardingProcessService onboardingProcessService;
 
+    private final OnboardingProcessService onboardingProcessService;
+    private final ProfileService profileService;
+    private final PromptGenerator promptGenerator;
 
     public OnboardingInfo.Process selectCategories(OnboardingCommand.SelectCategories command) {
         return onboardingProcessService.selectCategories(command);
@@ -22,11 +29,20 @@ public class OnboardingFacade {
     }
 
     public OnboardingInfo.Process submitInteractionStyle(OnboardingCommand.SubmitInteraction command) {
+        profileService.updateInteractionStyle(command.getUserId(), command.getInteractionStyle());
         return onboardingProcessService.submitInteractionStyle(command);
     }
 
-    public GeneratedCardBio generateInitCards(Long userId) {
-        return onboardingProcessService.generateCardBio(userId);
-        // TODO: 여기에서 프로필에 저장하거나, 프로필에서 온보딩 서비스 코드를 호출할 지 추후 결정
+    // TODO: 예외 처리 게선 고민해보기
+    public void generateInitCards(Long userId) {
+        String userPrompt = promptGenerator.generatePrompt(userId);
+        onboardingProcessService.generateCardBio(userPrompt)
+                .thenAccept(generatedBio -> {
+                    ProfileCommand.ApplyOnboardingResult command = GeneratedCardBio.toApplyOnboardingResult(generatedBio);
+                    profileService.applyOnboarding(userId, command);
+                }).exceptionally(ex -> {
+            log.error("AI 생성 중 오류 발생: {}", ex.getMessage());
+            return null;
+        });
     }
 }

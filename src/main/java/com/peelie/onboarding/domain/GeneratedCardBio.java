5e5c463c5ecc1e512a09c77b5768f0427a95a019
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-11 16:28:17 +0900

    feat: 생성된 카드와 한 줄소개를 프로필에 반영하기 위한 command로 변환하는 메서드 추가

--- src/main/java/com/peelie/onboarding/domain/GeneratedCardBio.java
+++ src/main/java/com/peelie/onboarding/domain/GeneratedCardBio.java
@@ -1,6 +1,8 @@
 package com.peelie.onboarding.domain;
 
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.peelie.profile.domain.Card;
+import com.peelie.profile.domain.ProfileCommand;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -48,5 +50,21 @@ public class GeneratedCardBio {
 
         @JsonPropertyDescription("한줄 소개 내용")
         private String bio;
+    }
+
+    // TODO: 프로필 커맨드를 여기서 참조하는게 맞는지 고민해보고 개선하기.
+    public static ProfileCommand.ApplyOnboardingResult toApplyOnboardingResult(GeneratedCardBio generatedBio) {
+        return ProfileCommand.ApplyOnboardingResult.builder()
+                .stage1Card(toCard(generatedBio.getStage1().getCard()))
+                .stage1Bio(generatedBio.getStage1().getBio().getBio())
+                .stage2Card(toCard(generatedBio.getStage2().getCard()))
+                .stage2Bio(generatedBio.getStage2().getBio().getBio())
+                .stage3Card(toCard(generatedBio.getStage3().getCard()))
+                .stage3Bio(generatedBio.getStage3().getBio().getBio())
+                .build();
+    }
+
+    public static Card toCard(GeneratedCardBio.InitCard initCard) {
+        return new Card(initCard.getTitle(), initCard.getSubTitle(), initCard.getContent());
     }
 }
